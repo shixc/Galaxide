c1ce77e94185475d45c6157541923603452cb9a0
loveyur <shixc0608@163.com>
2017-02-14 11:25:32 +0800

    星空战机初始代码

---
+++ Classes/Bmob/BmobSdk.h
@@ -0,0 +1,26 @@
+#ifndef __BMOB_SDK_H__
+#define __BMOB_SDK_H__
+
+#include "bmobcloud.h"
+#include "bmobobject.h"
+#include "bmobquery.h"
+#include "bmobqueryinterface.h"
+#include "bmobSDKinit.h"
+#include "bmobuser.h"
+
+#include "bmobclouddelegate.h"
+#include "bmobcountdelegate.h"
+#include "bmobdelegate.h"
+#include "bmobdeletedelegate.h"
+#include "bmobemailverifydelegate.h"
+#include "bmobfinddelegate.h"
+#include "bmobgetdelegate.h"
+#include "bmoblogindelegate.h"
+#include "bmobrequestSMScodedelegate.h"
+#include "bmobresetpasswordbycodedelegate.h"
+#include "bmobresetpassworddelegate.h"
+#include "bmobsavedelegate.h"
+#include "bmobstaticsdelegate.h"
+#include "bmobupdatedelegate.h"
+
+#endif //__BMOB_SDK_H__
